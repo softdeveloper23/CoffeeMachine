fb71a297179716b29c12edb3dd03a545f04889a1
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-27 14:19:40 +0200

    Added new variables and input code

--- src/main/java/machine/CoffeeMachine.java
+++ src/main/java/machine/CoffeeMachine.java
@@ -6,20 +6,35 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int water = 200;
-        int milk = 50;
-        int coffeeBeans = 15;
+        int reqWater = 200;
+        int reqMilk = 50;
+        int reqCoffeeBeans = 15;
+
+        int water = 0;
+        int milk = 0;
+        int coffeeBeans = 0;
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        water = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        milk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        coffeeBeans = scanner.nextInt();
 
         System.out.println("Write how many cups of coffee you will need:");
         int cups = scanner.nextInt();
         water = water * cups;
         milk = milk * cups;
-        coffeeBeans = coffeeBeans * cups;
+        reqCoffeeBeans = reqCoffeeBeans * cups;
 
         System.out.println("For " + cups + " cups of coffee you will need:");
         System.out.println(water + " ml of water");
         System.out.println(milk + " ml of milk");
-        System.out.println(coffeeBeans + " g of coffee beans");
+        System.out.println(reqCoffeeBeans + " g of coffee beans");
+
+
 
         System.out.println();
 
